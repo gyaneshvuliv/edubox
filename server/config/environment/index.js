822a4da553ae837fab2c0b14ef545e8b1194a7ba
gyaneshvuliv <vishal.garg@mobisign.co.in>
2021-03-01 17:22:13 +0530

    change port;

--- server/config/environment/index.js
+++ server/config/environment/index.js
@@ -19,7 +19,7 @@ var all = {
   root: path.normalize(__dirname + '/../../..'),
 
   // Server port
-  port: process.env.PORT || 8083,
+  port: process.env.PORT || 8088,
 
   // Server IP
   ip: process.env.IP || '0.0.0.0',
