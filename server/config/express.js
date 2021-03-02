64097ff28adc33779641c9c743297395ed1f9d64
gyaneshvuliv <vishal.garg@mobisign.co.in>
2021-03-02 18:07:52 +0530

    add limit

--- server/config/express.js
+++ server/config/express.js
@@ -36,7 +36,8 @@ module.exports = function(app) {
   app.use(methodOverride());
   app.use(cookieParser());
   app.use(passport.initialize());
-  app.use(express.limit(100000000));
+  app.use(express.bodyParser({limit: '500mb'}));
+  // app.use(express.limit(100000000));
 
 
   // app.use(multipartMiddleware);
